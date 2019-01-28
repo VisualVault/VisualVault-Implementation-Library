1821be3fceb453562503e05a240fc6a9c08b89e1
KendraAustin <40642159+KendraAustin@users.noreply.github.com>
2019-01-28 11:45:39 -0700

    Added error handling

--- Web Services/LibGroupGetGroupUserEmails/WebService_LibGroupGetGroupUserEmails.js
+++ Web Services/LibGroupGetGroupUserEmails/WebService_LibGroupGetGroupUserEmails.js
@@ -7,17 +7,30 @@ var groupsParamObj = [
 ];
 
 return vvClient.scripts.runWebService('LibGroupGetGroupUserEmails', groupsParamObj).then(function (userInfoResponse) {
-    var userInfo = userInfoResponse.data[2];
-    //Extract email information for use to send an email.  Place in a comma separated variable.
-    if (userInfo.hasOwnProperty('data')) {
-        userInfo.data.forEach(function (user) {
-            if (user.hasOwnProperty('emailAddress')) {
-                if (emailList != '') {
-                    emailList += ',';
-                }
-                emailList += user['emailAddress'];
+    if (userInfoResponse.meta.status === 200) {
+        if (userInfoResponse.hasOwnProperty('data')){
+            if (userInfoResponse.data[2] != 'undefined') {
+                var userInfo = userInfoResponse.data[2];
+                //Extract email information for use to send an email.  Place in a comma separated variable.
+                userInfo.forEach(function (user) {
+                    if (user.hasOwnProperty('emailAddress')) {
+                        if (emailList != '') {
+                            emailList += ',';
+                        }
+                        emailList += user['emailAddress'];
+                    }
+                });
             }
-
-        });
+            else {
+                throw new Error("The call to Get Group User Emails returned successfully, but the data could not be accessed.");
+            }
+        }
+        else {
+            throw new Error("The call to Get Group User Emails returned successfully, but the data was not returned.");
+        }
+    }
+    else {
+        throw new Error("An error was encountered when calling the Get Group User Emails process. The status returned was: " + userInfoResponse.meta.status +
+                        ". The status message returned was: " + userInfoResponse.meta.statusMsg);
     }
 });
