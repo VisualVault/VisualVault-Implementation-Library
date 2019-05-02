dba1c83244bf03eac4c6282cb42efa5ee2cb8c7b
KendraAustin <40642159+KendraAustin@users.noreply.github.com>
2019-05-02 12:44:19 -0700

    Create TestCommunications.js

---
+++ Web Services/TestCommunications.js
@@ -0,0 +1,31 @@
+var vvEntities = require("../VVRestApi");
+var logger = require('../log');
+
+module.exports.getCredentials = function () {
+    var options = {};
+    options.customerAlias = "CUSTOMERALIAS";
+    options.databaseAlias = "DATABASEALIAS";
+    options.userId = "USERID";
+    options.password = "PASSWORD";
+    options.clientId = "DEVELOPERKEY";
+    options.clientSecret = "DEVELOPERSECRET";
+    return options;
+};
+
+module.exports.main = function (vvClient, response) {
+    /*Script Name:   NodeJSTestCommunication
+     Customer:      Auersoft
+     Purpose:       Test to make sure communications between VV and NodeJS are working for new installations.    Need to replace the credentials aboe with valid information.  Setup as outside process, then as scheduled process.  Run as scheduled process.
+     Date of Dev:   12/24/2014
+     Last Rev Date: 
+
+     Revision Notes:
+     12/24/2014 - Jason:  First Setup of the script
+     */
+
+    logger.info('COMMUNICATION ARRIVED SUCCESSFULLY TO THE NODEJS SERVER.');
+
+    var responseMessage = 'COMMUNICATION ARRIVED SUCCESSFULLY.  THIS IS A CUSTOM MESSAGE COMING BACK FROM THE NODEJS SERVER.';
+
+    response.json(200, responseMessage);
+}
