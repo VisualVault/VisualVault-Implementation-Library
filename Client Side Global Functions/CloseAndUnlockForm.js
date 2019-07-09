56d8d5134cd69ec1c31cad32b1746db41aa05ed3
Max459 <48328403+Max459@users.noreply.github.com>
2019-07-09 14:07:09 -0700

    Update CloseAndUnlockForm.js
    updated to include DisplayConfirmMessaging

--- Client Side Global Functions/CloseAndUnlockForm.js
+++ Client Side Global Functions/CloseAndUnlockForm.js
@@ -1,3 +1,14 @@
 //the purpose of this function is to have a reusable function to close a form and unlock it as if they selected Close at the top of the screen.
+//HandleFormWindowClosing(true);
 
-HandleFormWindowClosing(true);
+var messagedata = 'Are you sure you would like to close this form? Any unsaved changes will be lost.'
+
+var okfunction= function () {
+    HandleFormWindowClosing(true);
+}
+
+var cancelfunction = function () {
+    return;
+}
+
+VV.Form.Global.DisplayConfirmMessaging(messagedata,'Close',okfunction,cancelfunction)
