4c8729e7fa486bebb42b001ffc018b69880c82e4
KendraAustin <40642159+KendraAustin@users.noreply.github.com>
2020-01-08 14:33:05 -0700

    Create btnEnable.js

---
+++ Standard Form Templates/User Management/btnEnable.js
@@ -0,0 +1,20 @@
+var validationResults = VV.Form.Template.FormValidation();
+
+if (validationResults == true) {
+
+    var okfunction = function () {
+        VV.Form.Template.EnableUser();
+    }
+
+    var cancelfunction = function () {
+        return;
+    }
+
+    //Display confirmation message
+    var messageData = 'You have indicated that this user account should be enabled. If you continue, this user will now be able to log in. Click OK to continue or Cancel to go back.';
+    var res = VV.Form.Global.DisplayConfirmMessaging(messageData, 'Confirm Action', okfunction, cancelfunction);
+}
+else {
+    var messageData = 'All of the fields have not been filled in completely or there is an issue with the range of the data entered.  Highlight your mouse over the red icon to see how you can resolve the error stopping you from saving this form.';
+    VV.Form.Global.DisplayMessaging(messageData, 'Missing Information');
+}
