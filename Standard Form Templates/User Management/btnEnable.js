1ffa98821fa93092f54aba7f2c69ae992d6402fb
KendraAustin <40642159+KendraAustin@users.noreply.github.com>
2020-01-22 14:11:14 -0700

    Update btnEnable.js

--- Standard Form Templates/User Management/btnEnable.js
+++ Standard Form Templates/User Management/btnEnable.js
@@ -11,7 +11,7 @@ if (validationResults == true) {
     }
 
     //Display confirmation message
-    var messageData = 'You have indicated that this user account should be enabled. If you continue, this user will now be able to log in. Click OK to continue or Cancel to go back.';
+    var messageData = 'You have indicated that this user account should be enabled. If you continue, this user will now be able to log in. Click OK to continue or Cancel to keep the user account disabled.';
     var res = VV.Form.Global.DisplayConfirmMessaging(messageData, 'Confirm Action', okfunction, cancelfunction);
 }
 else {
