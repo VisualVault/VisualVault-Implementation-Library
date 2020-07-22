34320ea8e91dde62335f3b3dd8257691c191dcb5
KendraAustin <40642159+KendraAustin@users.noreply.github.com>
2020-07-22 14:29:16 -0700

    Add Medicaid ID validations
    Also bring in new email reg and Michael's name regs.

--- Client Side Global Functions/SetupReg.js
+++ Client Side Global Functions/SetupReg.js
@@ -9,10 +9,12 @@
     Return Value:  The following represents the value being returned from this function:
                             
     Date of Dev:   
-    Last Rev Date: 04/15/2019
+    Last Rev Date: 07/22/2020
     Revision Notes:
     06/01/2011 - Jason Hatch: Initial creation of the business process. 
     04/15/2019 - Maxwell Rehbein: NumberOnly created. 
+    06/11/2020 - Michael Rainey: Add name validation RegExp
+	07/22/2020 - Kendra Austin: Add 2 Medicaid ID RegExp 
 */
 
 //This first PhoneReg is just for phones without extensions.  
@@ -23,7 +25,7 @@
 //VV.Form.Global.PhoneReg = new RegExp('^(\\s?)\\(([0-9]{3})\\)[ ]([0-9]{3})[-]([0-9]{4})?$');  
 //This PhoneReg is for phones that have extensions.  Format is (XXX) XXX-XXXX.  Needs to be x, ext or extension with any number of digits when entering the extensions.
 VV.Form.Global.PhoneReg = new RegExp('^(\\s?)\\(([0-9]{3})\\)[ ]([0-9]{3})[-]([0-9]{4})(?:\\s*(?:#|x\\.?|ex\\.?|ext\\.?|extension)\\s*(\\d+))?$');
-VV.Form.Global.EmailReg = new RegExp('\\b[A-Za-z0-9._%+-]+\@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b');
+VV.Form.Global.EmailReg = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 VV.Form.Global.URLReg = new RegExp('^([\\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$');
 VV.Form.Global.BlankReg = new RegExp('\\w');
 VV.Form.Global.ZipReg = new RegExp('^(\\s?)\\d{5}(-\\d{4})?$');
@@ -40,3 +42,9 @@ VV.Form.Global.Currency = new RegExp('^[0-9]*\.[0-9]{2}$');
 // Percent check. Value can be 0 - 100 including decimals.
 VV.Form.Global.Percent = new RegExp('^(100([\.][0]{1,})?$|[0-9]{1,2}([\.][0-9]{1,})?)$');
 VV.Form.Global.NumberOnly = new RegExp('^([0-9]{1,3}([0-9]{3},)*[0-9]{3}|[0-9]+)?$');
+//First and Last Name RegExp
+//To add additional characters to fail validation, add the special character or unicode between the two []
+VV.Form.Global.NameReg = new RegExp(/[!@#$%^*+={}/|\\\[\]<>?~]|[\d]{2,}/g);
+VV.Form.Global.NameNumberReg = new RegExp(/[\d]{1,}/g);
+VV.Form.Global.MedicaidIDRecipient = new RegExp('^\\d{10}$');
+VV.Form.Global.MedicaidIDProvider = new RegExp('^\\d{9}$');
