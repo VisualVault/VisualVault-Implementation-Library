52c96c1dd9cadc1528783c67b822805a090bf845
jasonhatch <jason.hatch@visualvault.com>
2022-10-14 15:10:02 -0700

    Update to ES6 Standard

--- Client Side Global Functions/DisplayConfirmMessagingV5.js
+++ Client Side Global Functions/DisplayConfirmMessagingV5.js
@@ -3,48 +3,58 @@ Script Name: DisplayConfirmMessaging
 Customer: VisualVault
 Purpose: The purpose of this function is to display a confirmation message modal.
 	Parameters: The following represent variables passed into the function:
-	Passed Parameters: messagedata, title, okfunction, cancelfunction
-						messagedata - HTML formatted string with the detailed message.
+	Passed Parameters: messageData, title, okFunction, cancelFunction
+						messageData - HTML formatted string with the detailed message.
 						title - Title applied to the confirmation dialog box.
-						okfunction - name of the function to call if the user selects OK.
-						cancelfunction - name of the function to call if the user selects cancel.
+						okFunction - name of the function to call if the user selects OK.
+						cancelFunction - name of the function to call if the user selects cancel.
 Return Value: The following represents the value being returned from this function:
-						@return {number} x raised to the n-th power.
+						-Returns no value
 Date of Dev:	06/01/2017
-Last Rev Date:	06/01/2017
+Last Rev Date:	06/07/2022
 Revision Notes:
-				06/01/2017 - Austin Noel: Initial creation of the business process. 
-				07/15/2021 - Jon Brown:   Update to work with VV version 5.
+		06/01/2017 - Austin Noel:           Initial creation of the business process. 
+		07/15/2021 - Jon Brown:             Update to work with VV version 5.
+                06/07/2022 - Franco Petosa Ayala:   Update to ES6
+                                                    The parameters were removed from the function which is triggered by the open event of dialog (Html element) as they were not used in the function
 */
 
-let confirmDialog = $(`<div id="dialog-confirm" title="${title}"></div>`)
+const confirmDialog = $(`<div id="dialog-confirm" title="${title}"></div>`)
 	.hide()
-	.append($(`<p>${messagedata}</p>`));
-let dialogStyle =$('<style id="dialog-confirm-style">body { overflow: hidden; }</style>');
+	.append($(`<p>${messageData}</p>`));
+const dialogStyle =$('<style id="dialog-confirm-style">body { overflow: hidden; }</style>');
 
 $('body').append(dialogStyle );
 $('body').append(confirmDialog);
 
 $('#dialog-confirm').dialog({
 	//animation settings when opening the dialog. Remove to have no animation
-    show: { effect: 'drop', direction: 'up', duration: 250}, 
+    show: { 
+		effect: 'drop', 
+		direction: 'up', 
+		duration: 250
+	}, 
 	//animation settings when closing the dialog. Remove to have no animation
-	hide: { effect: 'drop', direction: 'up', duration: 250},
+	hide: { 
+		effect: 'drop', 
+		direction: 'up', 
+		duration: 250
+	},
 	//buttons shown on modal
 	buttons: [
 		{
 			text:"OK",
 			click: function(){
 				try{
-					if(okfunction && typeof okfunction === 'function'){
-						okfunction(); 
-					}else if(okfunction && typeof okfunction === 'string'){
-						eval(okfunction);
+					if(okFunction && typeof okFunction === 'function'){
+						okFunction(); 
+					}else if(okFunction && typeof okFunction === 'string'){
+						eval(okFunction);
 					}
 				} catch (e){
-					console.error('Could not evaluate okfunction:',e);
+					console.error('Could not evaluate okFunction:',e);
 				}
-                                VV.Form.HideLoadingPanel();
+                VV.Form.HideLoadingPanel();
 				$(this).dialog('close');
 			},
 			class: 'k-button'
@@ -53,13 +63,13 @@ $('#dialog-confirm').dialog({
 			text:"Cancel",
 			click: function(){
 				try{
-					if(cancelfunction && typeof cancelfunction === 'function'){
-						cancelfunction(); 
-					}else if (cancelfunction && typeof cancelfunction === 'string'){
-						eval(cancelfunction);
+					if(cancelFunction && typeof cancelFunction === 'function'){
+						cancelFunction(); 
+					}else if (cancelFunction && typeof cancelFunction === 'string'){
+						eval(cancelFunction);
 					}
 				} catch (e){
-					console.error('Could not evaluate cancelfunction:',e);
+					console.error('Could not evaluate cancelFunction:',e);
 				}
 				$(this).dialog('close');
 			},
@@ -70,14 +80,14 @@ $('#dialog-confirm').dialog({
 			$(this).dialog('close').remove();
 			$(dialogStyle).remove();
 	},
-		open: function(event, ui) {
+	open: function() {
         $('.ui-dialog-titlebar-close')
             .addClass('k-button-icon k-button k-bare')
 			.html('<span class="k-icon">&#10006;</span>')
 			.css('position','absolute')
 			.css('right', '.3em');
         $('.ui-dialog-content').css('padding', '.5em 1em');
-		let overlay = $('ui-widget-overlay');
+		const overlay = $('ui-widget-overlay');
 		overlay.css('position', 'fixed')
 			.css('top', '0')
 			.css('left', '0')
