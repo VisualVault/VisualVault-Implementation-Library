a7100513f36546bab6144deebb340fbdc99d2897
jasonhatch <jason.hatch@visualvault.com>
2022-10-14 15:00:47 -0700

    Update to ES6 standards

--- Client Side Global Functions/FillinAndRelateForm.js
+++ Client Side Global Functions/FillinAndRelateForm.js
@@ -7,7 +7,7 @@
     Parameters:    The following represent variables passed into the function:  
                     templateId: Id of form template to fill in and relate to the current form instance
                     fieldMappings: array of objects containing: sourceFieldName, sourceFieldValue, targetFieldName
-                    launchType: 'self' launches the target form in the same window.
+                    launchType: 'self' launches the target form in the same window - 'blank' launches the target form in a new window.
                     Mapped field array example for build fieldMappings
                      var mappedField = {};
                      mappedField.sourceFieldName = 'providerId';
@@ -17,30 +17,32 @@
                    
     Return Value:  The following represents the value being returned from this function:
                             
-    Date of Dev:   
-    Last Rev Date: 08/21/2019
+    Date of Dev: 06/01/2017   
+    Last Rev Date: 06/20/2022
     Revision Notes:
     06/01/2017 - Tod Olsen: Initial creation of the business process. 
     01/03/2019 - Kendra Austin: Add launchType for option to redirect in the current window instead of opening new. 
     08/21/2019 - Jason Hatch:  Added mechanism to encode the URL so that it will change characters like a + sign in the email into an encoded value.
+    06/20/2022 - Franco Petosa Ayala: Update to ES6.
+                                      Changed the conditional structure (setting the launch mode) to improve code readability.
 */
 
 //Opens using new window that is popup blocker safe as long as this function is called from a DOM event handler
+let popupUrl = `${VV.BaseURL}form_details?formid=${templateId}&RelateForm=${VV.Form.DataID}&IsRelate=true&hidemenu=true`;
 
-var popupUrl = VV.BaseURL + "form_details?formid=" + templateId + "&RelateForm=" + VV.Form.DataID + "&IsRelate=true&hidemenu=true";
-
-fieldMappings.forEach(function (fieldMapping) {
+fieldMappings.forEach( fieldMapping => {
     if (fieldMapping) {
-        popupUrl += "&" + fieldMapping.targetFieldName + "=" + fieldMapping.sourceFieldValue;
+        //Concatenates the params to the url
+        popupUrl += `&${fieldMapping.targetFieldName}=${fieldMapping.sourceFieldValue}`;
     }
 }, this);
 
 //Determine action by the window that is launched.
-var launchMode = "_blank";
-if (typeof (launchType) != 'undefined') {
-    if (launchType.toLowerCase() == "self") {
-        launchMode = "_self";
-    }
+let launchMode = '';
+if ( typeof launchType != 'undefined' && launchType.toLowerCase() == "self") {
+    launchMode = "_self";
+} else{
+    launchMode = "_blank";
 }
 
 popupUrl = popupUrl.replace('+', '%2B');
@@ -48,7 +50,6 @@ popupUrl = popupUrl.replace('+', '%2B');
 if (launchMode == '_self') {
     VV.Form.ShowLoadingPanel();
     window.location = popupUrl;
-}
-else {
+} else {
     VV.Form.LastChildWindow = ShowPopUp(popupUrl, "", 900, 900, "yes", "yes", "no", "no", "no");
 }
