fa2b3fc4b571cc5f53d35dff4a8789556b2cdae4
jasonhatch <jason.hatch@visualvault.com>
2022-10-14 15:22:07 -0700

    Update for ES6 and better error handling.

--- Client Side Global Functions/FormatCurrency.js
+++ Client Side Global Functions/FormatCurrency.js
@@ -1,14 +1,45 @@
-//Purpose of function is to take a Currency entered in any format and format it into a standard of X.XX
+/*
+	Script Name:   FormatCurrency
+	Customer:      VisualVault
+	Purpose:       The purpose of this function is to format any currency passed as a parameter to: x.xx
+	Parameters:    The following represent variables passed into the function:
+				   -Currency: string value that represents the currency number
+                   e.g: '4.2267'
+				  
+	Return Value:  The following represents the value being returned from this function:
+                   currency: string value		
+				   or
+				   formatedCurrency: string value
 
-//parameter: Currency
-//Pass in a string that represents the Currency
+	Date of Dev: 06/21/2022
+	Last Rev Date: 06/21/2022
+	Revision Notes:
+				01/07/2020 - Kendra Austin:  Initial creation.
+				06/21/2022 - Franco Petosa Ayala: Update to ES6 and better error handling.
+*/
 
-//Convert string to integer
-var s2 = parseFloat(Currency.replace(/,/g, ''));
+let strArr = currency.split(''); // e.g: currency = 5.46 --> strArr = ['5','.','4','6']
 
-if (s2.toFixed(2) == 'NaN') {
-    return Currency;
-}
-else {
-    return s2.toFixed(2);
+// remove from array any not number caracter, except for '.'
+strArr = strArr.map(digit => {
+  if(!isNaN(digit) || digit == '.'){
+    return digit
+  }else{
+    return ''
+  }
+})
+
+// build up the string removing white spaces in case they are present
+let formatedCurrency = strArr.join('').trim()
+
+// verify the string can be converted to type number
+if(!isNaN(formatedCurrency) && formatedCurrency != ''){
+
+	// convert the string to number and apply the format x.xx
+   formatedCurrency = parseFloat(formatedCurrency).toFixed(2)
+   return formatedCurrency
+
+}else{
+	//as the passed value can not be formated it is returned unmodified
+	return currency
 }
